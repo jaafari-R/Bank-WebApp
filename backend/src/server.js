7d3823dc9a411aacf3422be6440793305ebfe215
jaafari-R <jaafariramadan@gmail.com>
2024-02-06 23:35:42 +0200

    Added cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 import transactionsRouter from "./routes/transaction.router.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
