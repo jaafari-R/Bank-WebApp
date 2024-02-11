dd19637d2e188c15f329277c06df8d36ec08fb9c
jaafari-R <jaafariramadan@gmail.com>
2024-02-11 07:05:01 +0200

    Added donenv configs

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,5 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export default class Config {
-    static PORT = 3100;
-    static DB_URL = "mongodb://127.0.0.1/bank";
-    static BALANCE_MINIMUM_VAL = 500;
+    static PORT = process.env.PORT // || 3100;
+    static DB_URL = process.env.DB_URL //  || "mongodb://127.0.0.1/bank";
+    static BALANCE_MINIMUM_VAL = process.env.BALANCE_MINIMUM_VAL // || 500;
 }
